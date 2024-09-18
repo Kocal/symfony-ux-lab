1636f650fa408f258c7a3bae92157679e15de432
Hugo Alliaume <hugo@alliau.me>
2024-09-18 11:07:19 +0200

    icons & autocast

---
+++ src/Controller/Icons2148Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class Icons2148Controller extends AbstractController
+{
+    #[Route('/icons2148', name: 'app_icons2148')]
+    public function index(): Response
+    {
+        return $this->render('icons2148/index.html.twig', [
+            'controller_name' => 'Icons2148Controller',
+        ]);
+    }
+}
